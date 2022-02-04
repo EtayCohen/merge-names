fd7ee10f05d8814d2e309af6385b040bc2374f16
Etay Cohen <etaychn@gmail.com>
2022-02-04 15:17:50 +0200

    implemented file parsing and added file.txt

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+import argparse
 
-names = [("Jacob", 15), ("Yaakov", 12), ("Tomer", 13), ("Tommer", 4), ("Sara", 19), ("Tommer", 10), ('Yaacov', 3)]
-synonyms = [("Jacob", "Yaakov"), ("Yaakov", "Yaacov"), ("Tommer", "Tomer"), ('Yaacov', 'Jacov')]
+
+def parse_file(filename):
+    names = []
+    synonyms = []
+    with open(filename, "r") as file:
+        names, synonyms = file.readlines()
+        names, synonyms = names.replace(' ', ''), synonyms.replace(
+            ' ', '').replace('(', '').replace(')', '')
+        names, synonyms = names[6::].split(','), synonyms[9::].split(',')
+        names = [(n[:n.index('(')], int(n[n.index('(') + 1:n.index(')')]))
+                 for n in names]
+        synonyms = list(zip(synonyms[::2], synonyms[1::2]))
+    return names, synonyms
 
 
 def merge_syns(synonyms):
@@ -30,16 +42,20 @@ def merge_names(names, synonyms):
                 flag = True
         if not flag:
             syns.append([{name}, frq])
-
-    for st, sm in syns:
-        print(st.pop(), sm)
+    print(', '.join([f"{st.pop()}({sm})" for st, sm in syns]))
 
 
 def main():
-    merge_names(names, synonyms)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', type=str, required=True)
+    args = parser.parse_args()
+    try:
+        merge_names(*parse_file(args.file))
+    except FileNotFoundError:
+        print("given file not found")
+    except Exception as e:
+        print("faild parsing given file")
 
 
 if __name__ == '__main__':
     main()
-
-
